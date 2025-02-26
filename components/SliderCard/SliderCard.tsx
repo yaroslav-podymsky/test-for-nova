987054690c95ea2133ac5edbb5d4c2e0350c79d1
yaroslav-podymsky <jarik9623@mail.ru>
2025-02-26 12:14:34 +0300

    fix-SliderCard

--- components/SliderCard/SliderCard.tsx
+++ components/SliderCard/SliderCard.tsx
@@ -1,19 +1,27 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./SliderCard.module.css"
 import Image from "next/image"
 import { TCardData } from "../SlickSlider/SlickSlider";
+import classNames from "classnames";
 
 
 type TProps = {} & TCardData
 
-const SliderCard: FC<TProps> = ({ img, subTitle, title }) => {
+const SliderCard: FC<TProps> = ({ img, subTitle, title, description }) => {
+    const [isShowScreen, setIsShowScreen] = useState(false)
+
     return (
-        <div className={styles.root}>
+        <div onMouseEnter={() => setIsShowScreen(true)} onMouseLeave={() => setIsShowScreen(false)} className={styles.root}>
+            <div className={classNames(styles.rootScreen, {
+            })}>
+            </div>
             <Image className={styles.img} src={img} alt=""></Image>
-            <div className={styles.descriptions}>
+            <div className={classNames(styles.descriptions, {
+                [styles.descriptionsSecondPosition]: isShowScreen
+            })}>
                 <h4 style={{ fontWeight: 600 }}>{title}</h4>
                 <h4>{subTitle}</h4>
-                {/* <p>{description}</p> */}
+                {isShowScreen && <p className={styles.description}>{description}</p>}
             </div>
         </div>
     )
